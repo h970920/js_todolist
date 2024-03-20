0afd7b505637197febe330a72ae872cd6ac67b8f
h970920 <h970920@naver.com>
2024-03-20 09:31:45 +0900

    js 오류 수정 ( defer) 명령어 추가

--- todo.js
+++ todo.js
@@ -1,6 +1,7 @@
 const addBtn = document.querySelector("#addBtn"); // 추가 버튼 선택
 
 function keyCodeCheck() {
+  const todoInput = document.querySelector("#todoInput");
   // 엔터키로 추가
   if (window.event.keyCode === 13 && todoInput.value !== "") {
     createTodo();
@@ -8,6 +9,7 @@ function keyCodeCheck() {
 }
 
 addBtn.addEventListener("click", () => {
+  const todoInput = document.querySelector("#todoInput");
   // + 버튼으로 추가
   if (todoInput.value !== "") {
     // 빈 값 입력 방지
@@ -16,12 +18,12 @@ addBtn.addEventListener("click", () => {
 });
 
 function createTodo() {
+  const todoInput = document.querySelector("#todoInput");
   // 할 일 추가 기능
   const todoList = document.querySelector("#todoList");
   const newLi = document.createElement("li"); // li 생성
   const newBtn = document.createElement("button"); // button 생성
   const newSpan = document.createElement("span"); // span 생성
-  const todoInput = document.querySelector("#todoInput");
 
   newLi.appendChild(newBtn); // li안에 button 담기
   newLi.appendChild(newSpan); // li안에 span 담기
