c5f2ffa1f89197e4aef3bd297b4f3a9dd06e02e9
h970920 <h970920@naver.com>
2024-03-20 09:18:07 +0900

    css 추가

---
+++ todo.js
@@ -0,0 +1,9 @@
+function keyCodeCheck() {
+  console.log(window.event);
+
+  if (window.event.keyCode === 13) {
+    const newLi = document.createElement("li"); // li 생성
+    const newBtn = document.createElement("button"); // button 생성
+    const newSpan = document.createElement("span"); // span 생성
+  }
+}
